fbd7498a115d6760623405a9691cb26957503872
Mauricio Salazar <emsd@kth.se>
2021-07-30 20:18:32 +0200

    Add file to pull docker images automatically

---
+++ pull_docker_images.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+docker pull salazaem/modules_mqtt_v2:dbmanager_x
+docker pull salazaem/modules_mqtt_v2:forecast_x
+docker pull salazaem/modules_mqtt_v2:battery_x
+docker pull salazaem/modules_mqtt_v2:controller_x
